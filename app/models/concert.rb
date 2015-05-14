8bce9fad5d9d3494c8a5168fe0921b7f24b01b04
kylemcco <kyle.p.mccormick@gmail.com>
2015-05-14 13:13:48 -0500

    added validators to Concert model

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -18,4 +18,34 @@ class Concert
     VALUES (?, ?, ?, ?, ?)", concert_info["artist"], concert_info["concert_date"], concert_info["venue"], concert_info["location"], concert_info["rating"])
   end
 
+  def self.save(concert_info)
+    Database.execute("INSERT INTO concerts (artist, concert_date, venue, location, rating)
+    VALUES (?, ?, ?, ?, ?)", concert_info["artist"], concert_info["concert_date"], concert_info["venue"], concert_info["location"], concert_info["rating"])
+  end
+
+  def self.validate_artist(input)
+    input.validate = lambda { |p| p != "" };
+    input.responses[:not_valid] = "Headliner cannot be empty."
+  end
+
+  def self.validate_date(input)
+    input.validate = /(0[1-9]|1[012])[- \/](0[1-9]|[12][0-9]|3[01])[- \/](19|20)\d\d/;
+    input.responses[:not_valid] = "Invalid date. Enter date in MM/DD/YYYY format."
+  end
+
+  def self.validate_venue(input)
+    input.validate = lambda { |p| p != "" };
+    input.responses[:not_valid] = "Venue cannot be empty."
+  end
+
+  def self.validate_location(input)
+    input.validate = lambda { |p| p != "" };
+    input.responses[:not_valid] = "Enter location in City, ST format (example: Nashville, TN)"
+  end
+
+  def self.validate_rating(input)
+    input.validate = lambda { |p| p.to_i.between?(1,10) };
+    input.responses[:not_valid] = "Rating must be a number 1-10."
+  end
+
 end
