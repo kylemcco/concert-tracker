b1f470bd92bd078f90a32f1ab90bf13bb6627e45
kylemcco <kyle.p.mccormick@gmail.com>
2015-05-18 13:47:33 -0500

    refactor to new app spec

--- app/controllers/concert_questions_controller.rb
+++ app/controllers/concert_questions_controller.rb
@@ -3,11 +3,7 @@ class ConcertQuestionsController
   def ask_questions
     concert_info = Hash.new
     say("Answer the following questions about the concert:\n")
-    concert_info[:artist] = ask("Who was the headlining act?") { |i| Concert.validate_artist(i) }
-    # openers_choice = ask("Were there any noteworthy openers? (y/n)")
-    # if openers_choice == "y"
-    #   openers = ask("List the opening act(s), seperated by commas (KISS,Meatloaf)").split(",")
-    # end
+    concert_info[:artist] = ask("Who was the artist?") { |i| Concert.validate_artist(i) }
     concert_info[:concert_date] = ask("When was the concert? (mm/dd/yyyy)") { |i| Concert.validate_date(i) }
     concert_info[:venue] = ask("What was the name of the venue?") { |i| Concert.validate_venue(i) }
     concert_info[:location] = ask("Where is the venue located? (City, ST)") { |i| Concert.validate_location(i) }
@@ -17,7 +13,6 @@ class ConcertQuestionsController
       puts "Concert saved"
       concerts_controller = ConcertsController.new
       concerts_controller.add(concert_info)
-      puts concert_info
     else
       puts "Let's try again"
     end
