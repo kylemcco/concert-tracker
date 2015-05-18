aa71585dfb51849ea2454c23a1c1c3017984f4d3
kylemcco <kyle.p.mccormick@gmail.com>
2015-05-18 11:47:33 -0500

    Venues model tests (copy of Concerts)

---
+++ test/models/test_venue.rb
@@ -0,0 +1,73 @@
+require_relative '../test_helper'
+
+describe Venue do
+
+  describe ".initialize" do
+    let(:venue){ Venue.new({:name=> "The Ryman", :city=>"Nashville", :state=>"TN"})}
+    it "sets concert attributes" do
+      assert_equal "The Ryman", venue.name
+      assert_equal "Nashville", venue.city
+      assert_equal "TN", venue.state
+    end
+  end
+
+  describe "#all" do
+    describe "if there are no venues in the database" do
+      it "should return an empty array" do
+        assert_equal [], Venue.all
+      end
+    end
+    describe "if there are venues" do
+      before do
+        Venue.new({:name=> "The Ryman", :city=>"Nashville", :state=>"TN"}.save
+        Venue.new({:name=> "Orange Peel", :city=>"Asheville", :state=>"SC"}.save
+        Venue.new({:name=> "Bijou Theater", :city=>"Knoxville", :state=>"TN"}.save
+      end
+      it "should return the venues in alphabetical order" do
+        expected = ["Bijou Theater", "Orange Peel", "The Ryman"]
+        actual = Venue.all.map{ |venue| venue.name }
+        assert_equal expected, actual
+      end
+      it "populates the returned venues' ids" do
+        expected_ids = Database.execute("SELECT id FROM venues order by name ASC").map{ |row| row['id'] }
+        actual_ids = Venue.all.map{ |venue| venue.id }
+        assert_equal expected_ids, actual_ids
+      end
+    end
+  end
+
+  describe "#count" do
+    describe "if there are no venues in the database" do
+      it "should return 0" do
+        assert_equal 0, Venue.count
+      end
+    end
+    describe "if there are venues" do
+      before do
+        Venue.new({:name=> "The Ryman", :city=>"Nashville", :state=>"TN"}.save
+        Venue.new({:name=> "Orange Peel", :city=>"Asheville", :state=>"SC"}.save
+        Venue.new({:name=> "Bijou Theater", :city=>"Knoxville", :state=>"TN"}.save
+      end
+      it "should return the correct count" do
+        assert_equal 3, Venue.count
+      end
+    end
+  end
+
+  describe ".save" do
+    describe "if we want to save a venue to the database" do
+      let(:venue){Venue.new({:name=> "The Ryman", :city=>"Nashville", :state=>"TN"})}
+      it "should add a venue" do
+        venue.save
+        assert_equal 1, Venue.count
+      end
+      it "should populate the model with id from the database" do
+        venue.save
+        last_row = Database.execute("SELECT * FROM venues")[0]
+        database_id = last_row['id']
+        assert_equal database_id, concert.id
+      end
+    end
+  end
+
+end
