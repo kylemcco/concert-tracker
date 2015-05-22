afec6cd512d1f48d560d5d8b52bb143cbd6c5ab1
kylemcco <kyle.p.mccormick@gmail.com>
2015-05-22 11:15:29 -0500

    Questions controller refactor

--- app/controllers/concert_questions_controller.rb
+++ app/controllers/concert_questions_controller.rb
@@ -1,45 +1,44 @@
 class ConcertQuestionsController
 
   def ask_questions
-    concert_info = Hash.new
-    venue_info = Hash.new
+    concert = Concert.new
+    venue = Venue.new
     say("Answer the following questions about the concert:\n")
-    concert_info[:artist] = Concert.validate_artist
-    concert_info[:concert_date] = Concert.validate_date
-    venue_info[:name] = Venue.validate_name
-    venue = Venue.find_by name: venue_info[:name]
-    if venue.nil?
-      ask_venue_questions(venue_info)
+    concert.artist = Concert.validate_artist
+    concert.concert_date = Concert.validate_date
+    venue.name = Venue.validate_name
+    if (Venue.find_by name: venue.name).nil?
+      ask_venue_questions(venue)
     else
       choose do |menu|
-        menu.prompt = "#{venue_info[:name]}, located in #{venue.city}, #{venue.state}, #{venue.country} found in database. Use this venue? (y/n)"
-        menu.choice(:y) { concert_info[:venue_id] = venue.id }
+        venue = Venue.find_by name: venue.name
+        menu.prompt = "#{venue.name}, located in #{venue.city}, #{venue.state}, #{venue.country} found in database. Use this venue? (y/n)"
+        menu.choice(:y) { concert.venue_id = venue.id }
         menu.choice(:n) {
-          venue_info[:name] = Venue.validate_name
-          ask_venue_questions(venue_info)
+          venue.name = Venue.validate_name
+          ask_venue_questions(venue)
         }
       end
     end
-    concert_info[:rating] = Concert.validate_rating
-    venue = Venue.find_by name: venue_info[:name]
-    confirmation = ask("Cool! You saw #{concert_info[:artist]} play #{venue.name} in #{venue.city} on #{concert_info[:concert_date]}. You gave this show a #{concert_info[:rating]}. Is that correct? (y/n)")
+    concert.rating = Concert.validate_rating
+    venue = Venue.find_by name: venue.name
+    confirmation = ask("Cool! You saw #{concert.artist} play #{venue.name} in #{venue.city} on #{concert.concert_date}. You gave this show a #{concert.rating}. Is that correct? (y/n)")
     if confirmation == "y"
-      puts "Concert saved\n"
-      concert_info[:venue_id] = venue.id
+      say("Concert saved\n")
+      concert.venue_id = venue.id
       concerts_controller = ConcertsController.new
-      concerts_controller.add(concert_info)
+      concert.save
       concerts_controller.list
     else
       main_menu_prompt
     end
   end
 
-  def ask_venue_questions(venue_info)
-    venue_info[:city] = Venue.validate_city
-    venue_info[:state] = Venue.validate_state
-    venue_info[:country] = Venue.validate_country
-    venues_controller = VenuesController.new
-    venues_controller.add(venue_info)
+  def ask_venue_questions(venue)
+    venue.city = Venue.validate_city
+    venue.state = Venue.validate_state
+    venue.country = Venue.validate_country
+    venue.save
   end
 
 end
